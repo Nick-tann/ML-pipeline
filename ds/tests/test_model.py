2c087ac0054812c9bdd517090450eba8aa756b4f
Nicholas <nictann96@gmail.com>
2024-11-27 15:07:35 +0800

    Added test for input validation

--- ds/tests/test_model.py
+++ ds/tests/test_model.py
@@ -1,2 +1,14 @@
-from ds import model
+import pytest
+import numpy as np
+from ds.model import validate_data
 
+def test_validate_data():
+    # Test input type
+    with pytest.raises(TypeError):
+        validate_data("test","test")
+    
+    #Test input size
+    with pytest.raises(ValueError):
+        X = np.ndarray(shape=(3,3))
+        y = np.ndarray(shape=(2,2))
+        validate_data(X,y)
